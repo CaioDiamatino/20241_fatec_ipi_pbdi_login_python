f2e41e48b47e57bccec37cf727dec8561d5bc68a
Jão <vitor40joao@gmail.com>
2024-03-12 10:09:12 -0300

    ap07(git-phyton): faz cadastro de usuario

--- login.py
+++ login.py
@@ -15,7 +15,7 @@ def existe(usuario):
   with psycopg.connect(
     host="localhost",
     port=5432,
-    dbname="20241_fatec_ipi_pbdi_login_python",
+    dbname="2024_fatec_ipi_pbdi_login_python",
     user="postgres",
     password="123456"
   ) as conexao:
@@ -29,7 +29,50 @@ def existe(usuario):
       #return True if result != None else False
       return result != None
     
-def teste():
-  print(existe(Usuario('admin', 'admin')))
+def cadastro(usuario):
+   with psycopg.connect(
+    host="localhost",
+    port=5432,
+    dbname="2024_fatec_ipi_pbdi_login_python",
+    user="postgres",
+    password="123456"
+  ) as conexao:
+    with conexao.cursor() as cursor:
+      cursor.execute(
+        'INSERT INTO tb_usuario(login, senha) VALUES (%s, %s);',
+        (usuario.login, usuario.senha)
+      )
+      print('Inserção concluida')
 
-teste()
+
+def menu():
+    texto = '0-Dair\n1-login\n2-logoff\n3-Inserir_login\n'
+    #Usuario ainda n existe
+    op = int(input(texto))
+    while op != 0:
+
+        if op == 1:
+            login = input('Digite o login')
+            senha = input('Digite a senha')
+            usuario = Usuario(login, senha)
+            #Expressão condicional (if/else de uma linha só)
+            print('Usuario OK' if existe(usuario) else "Usuario NOK")
+
+        elif op == 2:
+            usuario = None
+            print('Logoff realizado com sucesso')
+        elif op == 3:
+          login = input('Insira o usuario que deseja cadastrar')
+          senha = input('Digite a senha que deseja cadastrar')
+          usuario = Usuario(login, senha)
+          cadastro(usuario)
+
+        else:
+            print('Jovem somente valoentre 0, 1 e 2, pls')
+        op = int(input(texto))
+
+menu()
+# def teste():
+#   print(existe(Usuario('admin', 'admin')))
+
+# teste()
